d0e19fb85009b56dc328280cc1242560ea1fc58e
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-10-16 21:16:53 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emails = [
+    {email: 'bill@gates.com', important: true, subject: 'New Windows', content: 'Windows XI will launch in 2100.'},
+    {email: 'ada@lovelace.com', important: true, subject: 'Programming', content: 'Echantress of Numbers.'},
+    {email: 'john@carmac.com', important: false, subject: 'Updated Algo', content: 'New Algorithm for shadow volumes.'},
+    {email: 'gabe@gates.com', important: false, subject: 'HL3!', content: 'Just kidding...'},
+  ]
+  myColor = 'red';
 }
